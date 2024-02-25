9a7153301e6cc28a84139dd374596a39ae24b543
LOVE9573 <151997293+LOVE9573@users.noreply.github.com>
2024-02-25 17:14:36 +0530

    Update main.tf SG Module added

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -1,7 +1,7 @@
 # Configure AWS Provider
 provider "aws" {
     region  = var.region
-  }
+}
 
 # Create VPC
 module "vpc" {
@@ -15,4 +15,23 @@ module "vpc" {
     private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
     private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
     private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# Create nat-gateway
+module "nat_gateway" {
+    source                       = "../modules/nat-gateway"
+    public_subnet_az1_id         = module.vpc.public_subnet_az1_id
+    internet_gateway             = module.vpc.internet_gateway
+    public_subnet_az2_id         = module.vpc.public_subnet_az2_id
+    vpc_id                       = module.vpc.vpc_id
+    private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
+    private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+    private_data_subnet_az1_id   = module.vpc.private_data_subnet_az1_id
+    private_data_subnet_az2_id   = module.vpc.private_data_subnet_az2_id
+}
+
+# Create Security Group
+module "security_group" {
+    source                       = "../modules/security-group"
+    vpc_id                       = module.vpc.vpc_id
 }
